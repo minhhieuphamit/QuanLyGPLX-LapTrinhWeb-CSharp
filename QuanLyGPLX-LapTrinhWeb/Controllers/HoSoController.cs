6b26d38f181f5b52e59f76b4d5a3ffdbd85b5ac9
Pham Minh Hieu <minhhieupham2002@gmail.com>
2023-03-03 23:51:04 +0700

    Quản lý giấy phép lái xe

---
+++ QuanLyGPLX-LapTrinhWeb/Controllers/HoSoController.cs
@@ -0,0 +1,68 @@
+﻿using QuanLyGPLX_LapTrinhWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuanLyGPLX_LapTrinhWeb.Controllers
+{
+    public class HoSoController : Controller
+    {
+        // GET: HoSo
+        MyDataDataContext data = new MyDataDataContext();
+        public ActionResult DanhSachHoSo()
+        {
+            var all_HoSo = (from hs in data.HoSoGPLXes
+                            join ll in data.LyLiches on hs.SoCCCD equals ll.SoCCCD
+                            join px in data.PhuongXas on ll.DiaChi equals px.MaPX
+                            join qh in data.QuanHuyens on px.MaHuyen equals qh.MaHuyen
+                            join t in data.TinhTPs on qh.MaTinh equals t.MaTinh
+                            orderby ll.Ten, ll.HoLot ascending
+                            select new HoSo
+                            {
+                                MaGPLX = hs.MaGPLX,
+                                HoTen = ll.HoLot + " " + ll.Ten,
+                                NgaySinh = String.Format("{0:dd/MM/yyyy}", ll.NgaySinh),
+                                GioiTinh = ll.GioiTinh,
+                                DiaChi = px.TenPX + ", " + qh.TenHuyen + ", " + t.TenTinh,
+                                NgayCap = String.Format("{0:dd/MM/yyyy}", hs.NgayCapGPLX),
+                                NgayHetHan = String.Format("{0:dd/MM/yyyy}", hs.NgayHetHanGPLX),
+                                HangGPLX = hs.MaHang
+                            });
+            return View(all_HoSo);
+        }
+
+        public ActionResult Details(string id)
+        {
+            var D_HoSo = (from hs in data.HoSoGPLXes
+                          join ll in data.LyLiches on hs.SoCCCD equals ll.SoCCCD
+                          join px in data.PhuongXas on ll.DiaChi equals px.MaPX
+                          join qh in data.QuanHuyens on px.MaHuyen equals qh.MaHuyen
+                          join t in data.TinhTPs on qh.MaTinh equals t.MaTinh
+                          join dt in data.DanTocs on ll.MaDT equals dt.MaDT
+                          join qt in data.QuocTiches on ll.MaQT equals qt.MaQT
+                          join ttsh in data.TrungTamSatHaches on hs.MaTT equals ttsh.MaTT
+                          where hs.MaGPLX.Contains(id)
+                          select new HoSo
+                          {
+                              HinhAnh = ll.HinhAnh,
+                              MaGPLX = hs.MaGPLX,
+                              HoTen = ll.HoLot + " " + ll.Ten,
+                              NgaySinh = String.Format("{0:dd/MM/yyyy}", ll.NgaySinh),
+                              GioiTinh = ll.GioiTinh,
+                              SDT = ll.SDT,
+                              DanToc = dt.TenDT,
+                              DiaChi = px.TenPX + ", " + qh.TenHuyen + ", " + t.TenTinh,
+                              QuocTich = qt.TenQT,
+                              NgayCap = String.Format("{0:dd/MM/yyyy}", hs.NgayCapGPLX),
+                              NgayHetHan = String.Format("{0:dd/MM/yyyy}", hs.NgayHetHanGPLX),
+                              HangGPLX = hs.MaHang,
+                              DiemLT = hs.DiemLT.ToString(),
+                              DiemTH = hs.DiemTH.ToString(),
+                              TTSH = ttsh.TenTT
+                          }).First();
+            return View(D_HoSo);
+        }
+    }
+}
---
+++ QuanLyGPLX-LapTrinhWeb/Models/HoSo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuanLyGPLX_LapTrinhWeb.Models
+{
+    public class HoSo
+    {
+        public string HinhAnh { get; set; }
+        public string MaGPLX { get; set; }
+        public string HoTen { get; set; }
+        public string NgaySinh { get; set; }
+        public string GioiTinh { get; set; }
+        public string SDT { get; set; }
+        public string DanToc { get; set; }
+        public string DiaChi { get; set; }
+        public string QuocTich { get; set; }
+        public string NgayCap { get; set; }
+        public string NgayHetHan { get; set; }
+        public string HangGPLX { get; set; }
+        public string DiemLT { get; set; }
+        public string DiemTH { get; set; }
+        public string TTSH { get; set; }
+    }
+}
