0d8def0b4dbaf5d4c0111e8773a4c9ec005f923e
Pham Minh Hieu <minhhieupham2002@gmail.com>
2023-03-06 20:29:06 +0700

    Chuyen nhanh

--- QuanLyGPLX-LapTrinhWeb/Controllers/HoSoController.cs
+++ QuanLyGPLX-LapTrinhWeb/Controllers/HoSoController.cs
@@ -64,5 +64,38 @@ namespace QuanLyGPLX_LapTrinhWeb.Controllers
                           }).First();
             return View(D_HoSo);
         }
+
+        public ActionResult Edit(string id)
+        {
+            var E_MaGPLX = data.HoSoGPLXes.First(m => m.MaGPLX == id);
+            return View(E_MaGPLX);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(string id, FormCollection collection)
+        {
+            var E_MaGPLX = data.HoSoGPLXes.First(m => m.MaGPLX == id);
+            var E_MaHang = collection["MaHang"];
+            var E_NgayCapGPLX = collection["NgayCapGPlx"];
+            var E_NgayHetHanGPLX = collection["NgayHetHanGPLX"];
+            var E_DiemLT = collection["DiemLT"];
+            var E_DiemTH = collection["DiemTH"];
+            var E_MaTT = collection["MaTT"];
+            if (E_MaGPLX == null)
+            {
+                ViewData["Error"] = "Don't empty";
+            }
+            else
+            {
+                E_MaGPLX.MaHang = E_MaHang;
+                E_MaGPLX.NgayCapGPLX = DateTime.Parse(E_NgayCapGPLX);
+                E_MaGPLX.NgayHetHanGPLX = DateTime.Parse(E_NgayHetHanGPLX);
+                E_MaGPLX.DiemLT = int.Parse(E_DiemLT);
+                E_MaGPLX.DiemTH = int.Parse(E_DiemTH);
+                E_MaGPLX.MaTT = E_MaTT;
+                return RedirectToAction("DanhSachHoSo");
+            }
+            return this.Edit(id);
+        }
     }
 }
